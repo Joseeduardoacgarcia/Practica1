eb13a22258aa5f4b954fac77d90c050eead23946
joseeduardoacgarcia <joseeduardoacgarcia@gmail.com>
2024-02-27 10:08:51 -0600

    Cambio 1

---
+++ Nombres.java
@@ -0,0 +1,13 @@
+package Repositorio;
+
+import java.util.Scanner;
+
+public class Nombres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Jose");
+	}
+
+}
